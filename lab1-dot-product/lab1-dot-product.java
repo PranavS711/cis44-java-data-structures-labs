fbffebbadb7842b185f06f0310882aded07dadcf
Pranav Saravanan <114972524+PranavS711@users.noreply.github.com>
2026-01-30 19:20:28 -0800

    Implement dot product calculation for two arrays

--- lab1-dot-product/lab1-dot-product.java
+++ lab1-dot-product/lab1-dot-product.java
@@ -1,1 +1,30 @@
+import java.util.Random;
+import java.util.Arrays;
+
+public class DotProductArrays {
+    public static void main(String[] args) {
+        int n = 5;                       
+        int[] a = new int[n];            
+        int[] b = new int[n];
+        int[] c = new int[n];            
+
+        Random rand = new Random();      
+
+       
+        for (int i = 0; i < n; i++) {
+            a[i] = rand.nextInt(21) - 10;   
+            b[i] = rand.nextInt(21) - 10;
+        }
+
+       
+        for (int i = 0; i < n; i++) {
+            c[i] = a[i] * b[i];
+        }
+
+        
+        System.out.println("Array a: " + Arrays.toString(a));
+        System.out.println("Array b: " + Arrays.toString(b));
+        System.out.println("Array c (a[i] * b[i]): " + Arrays.toString(c));
+    }
+}
 
