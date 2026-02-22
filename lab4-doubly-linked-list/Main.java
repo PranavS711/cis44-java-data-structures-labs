c1dd227ec58624d61fe8898160b5ee8133697be7
Pranav Saravanan <114972524+PranavS711@users.noreply.github.com>
2026-02-22 12:08:09 -0800

    Add text editor functionality in Main.java
    Implemented a text editor with options to type text, undo, redo, and print current text.

--- lab4-doubly-linked-list/Main.java
+++ lab4-doubly-linked-list/Main.java
@@ -1,1 +1,46 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        TextEditor editor = new TextEditor();
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\nText Editor");
+            System.out.println("1 Type text");
+            System.out.println("2 Undo");
+            System.out.println("3 Redo");
+            System.out.println("4 Print current");
+            System.out.println("0 Exit");
+            System.out.print("Choose: ");
+
+            String choice = sc.nextLine().trim();
+
+            if (choice.equals("0")) {
+                System.out.println("Goodbye");
+                break;
+            } else if (choice.equals("1")) {
+                System.out.print("Type something to add: ");
+                String add = sc.nextLine();
+                editor.add(add);
+                editor.printCurrent();
+
+            } else if (choice.equals("2")) {
+                editor.undo();
+                editor.printCurrent();
+
+            } else if (choice.equals("3")) {
+                editor.redo();
+                editor.printCurrent();
+
+            } else if (choice.equals("4")) {
+                editor.printCurrent();
+
+            } else {
+                System.out.println("Invalid choice");
+            }
+        }
+
+        sc.close();
+    }
+}
