8d0ba434c03bc860bd0e20ad107f70125af605bd
Pranav Saravanan <114972524+PranavS711@users.noreply.github.com>
2026-02-28 13:44:01 -0800

    Implement travel itinerary management in Main.java
    Implemented a main method to manage a travel itinerary using a LinkedPositionalList. Added stops, inserted a stop, and modified an existing stop, demonstrating the use of the list's methods.

--- lab6-positional-list/Main.java
+++ lab6-positional-list/Main.java
@@ -1,1 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        LinkedPositionalList<String> itinerary = new LinkedPositionalList<>();
 
+        // add the main stops
+        Position<String> paris = itinerary.addLast("Eiffel Tower");
+        Position<String> rome = itinerary.addLast("Colosseum");
+        itinerary.addLast("Big Ben");
+
+        // insert a stop in between
+        itinerary.addAfter(paris, "Louvre Museum");
+
+        // change one stop just to show set works
+        itinerary.set(rome, "Roman Forum");
+
+        System.out.println("Final itinerary:");
+
+        // this for each loop proves the iterator works
+        for (String stop : itinerary) {
+            System.out.println(stop);
+        }
+    }
+}
