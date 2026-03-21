3f430621c15aa04cfa8e30ba8d897242d91f4b00
Pranav Saravanan <114972524+PranavS711@users.noreply.github.com>
2026-03-21 12:54:26 -0700

    Implement sorting algorithms: selection, insertion, merge

---
+++ lab8-sorting-race/SortingAlgorithms.java
@@ -0,0 +1,76 @@
+public class SortingAlgorithms {
+
+    // Selection Sort: O(n^2)
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // swap
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    // Insertion Sort: O(n^2), Best: O(n)
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+    }
+
+    // Merge Sort: O(n log n)
+    public static void mergeSort(int[] arr) {
+        if (arr == null || arr.length <= 1) return;
+        int[] temp = new int[arr.length];
+        mergeSortRecursive(arr, temp, 0, arr.length - 1);
+    }
+
+    private static void mergeSortRecursive(int[] arr, int[] temp, int left, int right) {
+        if (left >= right) return;
+
+        int mid = (left + right) / 2;
+
+        mergeSortRecursive(arr, temp, left, mid);
+        mergeSortRecursive(arr, temp, mid + 1, right);
+
+        merge(arr, temp, left, mid, right);
+    }
+
+    private static void merge(int[] arr, int[] temp, int left, int mid, int right) {
+        int i = left;
+        int j = mid + 1;
+        int k = left;
+
+        while (i <= mid && j <= right) {
+            if (arr[i] <= arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+            }
+        }
+
+        while (i <= mid) temp[k++] = arr[i++];
+        while (j <= right) temp[k++] = arr[j++];
+
+        for (int x = left; x <= right; x++) {
+            arr[x] = temp[x];
+        }
+    }
+}
