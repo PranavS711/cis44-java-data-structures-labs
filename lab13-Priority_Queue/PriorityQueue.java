1f8942fee6119ba0d085247a79d6ef339b85dfab
Pranav Saravanan <114972524+PranavS711@users.noreply.github.com>
2026-05-07 08:08:55 -0700

    Add PriorityQueue interface with basic methods

--- lab13-Priority_Queue/PriorityQueue.java
+++ lab13-Priority_Queue/PriorityQueue.java
@@ -1,1 +1,14 @@
+public interface PriorityQueue<K extends Comparable<K>, V> {
 
+    // inserts a new entry
+    void insert(K key, V value);
+
+    // removes smallest key
+    MyEntry<K, V> removeMin();
+
+    // gets smallest key
+    MyEntry<K, V> min();
+
+    // checks if queue is empty
+    boolean isEmpty();
+}
