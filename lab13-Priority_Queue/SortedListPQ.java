645092aedb2830a6f1370863955653435b77ca22
Pranav Saravanan <114972524+PranavS711@users.noreply.github.com>
2026-05-07 08:09:54 -0700

    Implement SortedListPQ with insert and removeMin methods

--- lab13-Priority_Queue/SortedListPQ.java
+++ lab13-Priority_Queue/SortedListPQ.java
@@ -1,1 +1,55 @@
+import java.util.LinkedList;
 
+public class SortedListPQ<K extends Comparable<K>, V>
+        implements PriorityQueue<K, V> {
+
+    // linked list to store entries
+    private LinkedList<MyEntry<K, V>> list = new LinkedList<>();
+
+    // checks if empty
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    // inserts in sorted order
+    // O(n)
+    public void insert(K key, V value) {
+
+        MyEntry<K, V> newEntry = new MyEntry<>(key, value);
+
+        // loop through list
+        for (int i = 0; i < list.size(); i++) {
+
+            // insert before bigger key
+            if (newEntry.compareTo(list.get(i)) < 0) {
+                list.add(i, newEntry);
+                return;
+            }
+        }
+
+        // add to end if biggest
+        list.addLast(newEntry);
+    }
+
+    // gets smallest entry
+    // O(1)
+    public MyEntry<K, V> min() {
+
+        if (isEmpty()) {
+            return null;
+        }
+
+        return list.getFirst();
+    }
+
+    // removes smallest entry
+    // O(1)
+    public MyEntry<K, V> removeMin() {
+
+        if (isEmpty()) {
+            return null;
+        }
+
+        return list.removeFirst();
+    }
+}
