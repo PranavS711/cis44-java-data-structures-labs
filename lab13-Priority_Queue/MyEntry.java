ae57065bbd440fadd7c3799b5f3abc4948a653f2
Pranav Saravanan <114972524+PranavS711@users.noreply.github.com>
2026-05-07 08:08:29 -0700

    Implement MyEntry class for key-value pairs

--- lab13-Priority_Queue/MyEntry.java
+++ lab13-Priority_Queue/MyEntry.java
@@ -1,1 +1,36 @@
+public class MyEntry<K extends Comparable<K>, V> implements Comparable<MyEntry<K, V>> {
 
+    // stores the key
+    private K key;
+
+    // stores the value
+    private V value;
+
+    // constructor
+    public MyEntry(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    // returns the key
+    public K getKey() {
+        return key;
+    }
+
+    // returns the value
+    public V getValue() {
+        return value;
+    }
+
+    // compares keys
+    @Override
+    public int compareTo(MyEntry<K, V> other) {
+        return this.key.compareTo(other.key);
+    }
+
+    // prints entry nicely
+    @Override
+    public String toString() {
+        return "(" + key + ", " + value + ")";
+    }
+}
